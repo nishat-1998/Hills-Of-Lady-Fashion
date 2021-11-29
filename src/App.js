24dacafbc32d4157b570e58606abe7c305a66230
Nishat Ahmed <nishatahmed4485@gmail.com>
2021-11-29 14:52:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+    products:data.products,
+    size:"",
+    sort:""
+    };
+    }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+   <header className='grid-container'>
+     Lady Fashion
+     </header>
+    <main>
+    <div className="content">
+      <div className="main">
+    <Products products={this.state.products}> </Products>
+        </div>
+<div className="sidebar"> 
+Cart Items
+</div>
+    </div>
+    </main>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import formatCurrency from '../Util';
+
+
+export default class Products extends Component {
+   
+    render() {
+        return (
+            <div>
+                <ul className="products">
+           {this.props.products.map((product) => (
+               <li key={product._id}>
+               <div className="product">
+                <a href={"#" + product._id}>
+                    <img src={product.image} alt={product.title}></img>
+               </a>
+               <p>
+                   {product.title}
+               </p>
+               <div className="product-price">
+                   <div>
+                       { formatCurrency(product.price)}
+                       </div>
+                       <button className="button-primary">Add To Cart</button>
+                   </div>
+               </div>
+               </li>
+           ))}
+                </ul>
+            </div>
+        )
+    }
+}
